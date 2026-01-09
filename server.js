e8d8939ab09265584a2ee56a2a82a5fc1d83c059
Tatrace <tatirookami@gmail.com>
2026-01-08 21:40:31 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,28 +1,27 @@
-import express from "express";
-import http from "http";
-import { Server } from "socket.io";
+const express = require("express");
+const http = require("http");
+const path = require("path");
+const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
-const states = {};
+app.get("/", (req, res) => {
+  res.redirect("/login.html");
+});
 
 io.on("connection", socket => {
+  console.log("Conectado:", socket.id);
 
-  socket.on("controller:update", ({ id, data }) => {
-    states[id] = data;
+  socket.on("state:update", state => {
+    socket.broadcast.emit("state:update", state);
   });
-
-  socket.on("hud:get", id => {
-    socket.emit("hud:state", states[id] || null);
-  });
-
 });
 
-server.listen(3000, () => {
-  console.log("Servidor rodando");
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log("Servidor rodando na porta", PORT);
 });
-
